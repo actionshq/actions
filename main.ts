cd2e1ef93a7ac7b7f949ce92d3e3d5e6d3db5311
Marek Brieger <m.brieger@codevoy.com>
2023-01-23 19:21:10 +0000

    refactor: use readdir promise

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { readdir, writeFileSync, readFileSync, existsSync, mkdirSync } from "node:fs"
+import { writeFileSync, readFileSync, existsSync, mkdirSync } from "node:fs"
+import { readdir,  } from "node:fs/promises"
 import { join, dirname } from "node:path"
 import { fileURLToPath } from "node:url"
 import { load } from "js-yaml"
@@ -9,19 +10,19 @@ const __dirname = dirname(__filename);
 const outputPath = join(__dirname, 'dist/actions');
 const actionsPath = join(__dirname, 'actions');
 
-readdir(actionsPath, (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-        const inputFilePath = join(actionsPath, file)
-        const outputFilePath = join(outputPath, file.replace('.yaml', '.json'))
-        const action = load(readFileSync(inputFilePath, 'utf8')) as any;
-        const fileName = file.split(".")[0]
-        if (action.spec.slug == null) {
-            action.spec = {
-                ...action.spec,
-                slug: fileName
-            }
+if (!existsSync(outputPath)) mkdirSync(outputPath, { recursive: true });
+
+const actionFiles = await readdir(actionsPath)
+actionFiles.forEach(file => {
+    const inputFilePath = join(actionsPath, file)
+    const outputFilePath = join(outputPath, file.replace('.yaml', '.json'))
+    const action = load(readFileSync(inputFilePath, 'utf8')) as any;
+    const fileName = file.split(".")[0]
+    if (action.spec.slug == null) {
+        action.spec = {
+            ...action.spec,
+            slug: fileName
         }
-        writeFileSync(outputFilePath, JSON.stringify(action));
-    });
+    }
+    writeFileSync(outputFilePath, JSON.stringify(action));
 });
